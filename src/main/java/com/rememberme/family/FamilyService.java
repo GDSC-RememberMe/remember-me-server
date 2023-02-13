7ec6497adbeda849a59c0c4774e8b5c16c0a563d
sooyoungh <sooyoung.h8@gmail.com>
2023-02-13 18:59:51 +0900

    ✨ feat: 환자 검색 및 가족 관계 설정하는 기능 구현

---
+++ src/main/java/com/rememberme/family/FamilyService.java
@@ -0,0 +1,40 @@
+package com.rememberme.family;
+
+import com.rememberme.family.dto.FamilyResponseDto;
+import com.rememberme.family.entity.Family;
+import com.rememberme.user.entity.User;
+import com.rememberme.user.entity.enumType.Role;
+import com.rememberme.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FamilyService {
+
+    private final FamilyRepository familyRepository;
+    private final UserRepository userRepository;
+
+    public List<FamilyResponseDto> searchFamilyByKeyword(String keyword) {
+        List<User> userList = userRepository.findByUsernameContaining(keyword);
+
+        return userList.stream()
+                .filter(user -> user.getRole().equals(Role.PATIENT))
+                .map(user -> new FamilyResponseDto().of(user))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public void setFamilyOfCaregiverUser(Long userId, Long familyId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("해당 보호자 회원이 존재하지 않습니다."));
+        Family family = familyRepository.findById(familyId)
+                .orElseThrow(() -> new RuntimeException("해당 환자 회원이 존재하지 않습니다."));
+
+        user.saveFamily(family);
+    }
+}
