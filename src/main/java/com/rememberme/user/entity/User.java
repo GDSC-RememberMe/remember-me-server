bc7df85ea84dbf34558d8a046de6143be01da0cf
sooyoungh <sooyoung.h8@gmail.com>
2023-03-06 19:00:57 +0900

    🐛 fix(User): 키 자동생성 설정 추가

--- src/main/java/com/rememberme/user/entity/User.java
+++ src/main/java/com/rememberme/user/entity/User.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
