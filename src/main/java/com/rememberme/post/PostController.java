800819898517ab28fed37e4d6250c5d080ed3213
sooyoungh <sooyoung.h8@gmail.com>
2023-03-07 20:06:18 +0900

    ✨ feat: 게시글 저장/삭제/수정/조회 기능 구현

---
+++ src/main/java/com/rememberme/post/PostController.java
@@ -0,0 +1,56 @@
+package com.rememberme.post;
+
+import com.rememberme.post.dto.PostAllResponseDto;
+import com.rememberme.post.dto.PostOneResponseDto;
+import com.rememberme.post.dto.PostSaveRequestDto;
+import com.rememberme.post.dto.PostUpdateRequestDto;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/post")
+public class PostController {
+
+    private final PostService postService;
+
+    @ApiOperation(value = "게시글 저장")
+    @PostMapping
+    public ResponseEntity createPost(
+            Authentication authentication,
+            @RequestBody PostSaveRequestDto postSaveRequestDto) {
+        Long memberId = Long.parseLong(authentication.getName());
+        postService.createPost(memberId, postSaveRequestDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "게시글 수정")
+    @PatchMapping
+    public ResponseEntity updatePost(
+            Authentication authentication,
+            @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
+        Long memberId = Long.parseLong(authentication.getName());
+        postService.updatePost(memberId, postUpdateRequestDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "게시글 전체 조회")
+    @GetMapping
+    public ResponseEntity<List<PostAllResponseDto>> getPostAll() {
+        List<PostAllResponseDto> posts = postService.getPostAll();
+        return ResponseEntity.ok(posts);
+    }
+
+    @ApiOperation(value = "게시글 개별 조회")
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostOneResponseDto> getPostOneByPostId(@PathVariable Long postId) {
+        PostOneResponseDto postOneResponseDto = postService.getPostOneByPostId(postId);
+        return ResponseEntity.ok(postOneResponseDto);
+    }
+}
---
+++ src/main/java/com/rememberme/post/dto/PostSaveRequestDto.java
@@ -0,0 +1,13 @@
+package com.rememberme.post.dto;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostSaveRequestDto {
+
+    private String title;
+
+    private String contents;
+}
