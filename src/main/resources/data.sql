ed3047a1c6d20a20ce1ca93c377b8665e38954e6
sooyoungh <sooyoung.h8@gmail.com>
2023-03-09 20:23:31 +0900

    ✨ feat: '추억의 물건' 게임 DB 샘플 데이터 저장

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--CREATE SCHEMA IF NOT EXISTS `rmdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
+
+USE `rmdb` ;
+INSERT INTO nostalgia_item (title, img_url) VALUES ('공중전화', 'https://user-images.githubusercontent.com/77563814/223988764-0bfb3eb5-0159-4462-a7bb-8045dfab762f.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('도시락', 'https://user-images.githubusercontent.com/77563814/223988850-36506d37-0c29-47cb-84d1-e1a51da9db44.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('전화기', 'https://user-images.githubusercontent.com/77563814/223988860-138b801f-6014-4717-86b7-2b5224a10e86.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('카세트', 'https://user-images.githubusercontent.com/77563814/223989114-cbbcbd1c-39e3-4389-a5e1-f4938a4cbbb7.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('달려라 하니', 'https://user-images.githubusercontent.com/77563814/223989130-acdec9be-7034-4dc9-ac87-755f84bd42c3.png');
