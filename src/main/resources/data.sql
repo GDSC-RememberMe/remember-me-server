88c4b15239a4b38c5ac0f8690b6e5e85056ab997
sooyoungh <sooyoung.h8@gmail.com>
2023-03-16 22:13:00 +0900

    ➕ chore: NostalgiaItem 예시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,23 @@
 --CREATE SCHEMA IF NOT EXISTS `rmdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
 
 USE `rmdb` ;
-INSERT INTO nostalgia_item (title, img_url) VALUES ('공중전화', 'https://user-images.githubusercontent.com/77563814/223988764-0bfb3eb5-0159-4462-a7bb-8045dfab762f.png');
-INSERT INTO nostalgia_item (title, img_url) VALUES ('도시락', 'https://user-images.githubusercontent.com/77563814/223988850-36506d37-0c29-47cb-84d1-e1a51da9db44.png');
-INSERT INTO nostalgia_item (title, img_url) VALUES ('전화기', 'https://user-images.githubusercontent.com/77563814/223988860-138b801f-6014-4717-86b7-2b5224a10e86.png');
-INSERT INTO nostalgia_item (title, img_url) VALUES ('카세트', 'https://user-images.githubusercontent.com/77563814/223989114-cbbcbd1c-39e3-4389-a5e1-f4938a4cbbb7.png');
-INSERT INTO nostalgia_item (title, img_url) VALUES ('달려라 하니', 'https://user-images.githubusercontent.com/77563814/223989130-acdec9be-7034-4dc9-ac87-755f84bd42c3.png');
+
+-- nostalgia_item
+INSERT INTO nostalgia_item (title, img_url) VALUES ('도시락', 'https://user-images.githubusercontent.com/77563814/224518194-b45c1103-1de2-4e7d-9267-66ae4637c53a.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('못난이', 'https://user-images.githubusercontent.com/77563814/224518214-fd6cc6b1-b434-4401-aa24-be2b320bcb95.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('뽀빠이', 'https://user-images.githubusercontent.com/77563814/224518227-b0e1763f-4759-424b-86a7-eec9b6ec8b9d.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('하니만화', 'https://user-images.githubusercontent.com/77563814/224518255-7c3507a3-61b0-44a6-bea1-fba0ef1205b2.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('크레파스', 'https://user-images.githubusercontent.com/77563814/224518247-42e48290-c9e6-4746-9b4f-c9d650fc2682.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('사이다', 'https://user-images.githubusercontent.com/77563814/224518223-b9b2d0e1-8724-4394-b865-c4063a616d90.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('축음기', 'https://user-images.githubusercontent.com/77563814/224518244-556547ad-33d0-49cd-a326-414007ddfa5d.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('전화기', 'https://user-images.githubusercontent.com/77563814/224518236-d7b91ef0-6a23-4197-9922-2495c6e399e1.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('보석함', 'https://user-images.githubusercontent.com/77563814/224518233-1754aa30-3751-4b85-a54a-6141b24e44c9.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('빨래', 'https://user-images.githubusercontent.com/77563814/224518230-ad2ef68f-c6a9-44ad-bb27-5189d9338568.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('카세트', 'https://user-images.githubusercontent.com/77563814/224518246-69a88783-af07-4006-9b85-6a9cbcd25f16.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('공중전화', 'https://user-images.githubusercontent.com/77563814/224518191-e1692288-6a76-439e-a835-fb6d44ee32e4.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('사탕', 'https://user-images.githubusercontent.com/77563814/224518221-2d82e9b9-7ec0-4b2f-a513-c357a9321e03.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('옥수수빵', 'https://user-images.githubusercontent.com/77563814/224518219-0abc87f3-7467-438a-aaf9-ae1a8530279c.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('메주', 'https://user-images.githubusercontent.com/77563814/224518218-484d4dae-3a09-45a0-92af-7ac14373d945.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('게임기', 'https://user-images.githubusercontent.com/77563814/224518175-838008e0-b96d-4d84-a65a-8f67c2bab413.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('티비', 'https://user-images.githubusercontent.com/77563814/224518254-88cd4f06-fd0d-48f1-b7ae-ffb569b6cec9.png');
+INSERT INTO nostalgia_item (title, img_url) VALUES ('타자기', 'https://user-images.githubusercontent.com/77563814/224518251-585c4708-6e9f-4b71-826a-a00c54bde45e.png');
