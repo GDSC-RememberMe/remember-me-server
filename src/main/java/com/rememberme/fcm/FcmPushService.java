d78146b4e95b5164a876ade4bfe1c10bd6a33ed6
sooyoungh <sooyoung.h8@gmail.com>
2023-03-21 20:59:04 +0900

    ✨ feat: 불필요한 Dto 삭제

--- src/main/java/com/rememberme/fcm/FcmPushService.java
+++ src/main/java/com/rememberme/fcm/FcmPushService.java
@@ -7,7 +7,6 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
-import com.rememberme.fcm.dto.FcmPushDto;
 import com.rememberme.memory.MemoryService;
 import com.rememberme.memory.dto.MemoryRandomResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -30,12 +29,12 @@ public class FcmPushService {
     @Value("${fcm.key.path}")
     private String FCM_PRIVATE_KEY;
 
-    // @Value("${fcm.key.scope}")
     private String FIREBASE_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
 
     private FirebaseMessaging firebaseMessaging;
 
     private final MemoryService memoryService;
+    private final String PUSH_QUESTION = " 추억이 기억나시나요?";
 
     // 1. fcm 기본 설정 진행
     @PostConstruct // 의존성 주입 후, 실행
@@ -54,46 +53,49 @@ public class FcmPushService {
         this.firebaseMessaging = FirebaseMessaging.getInstance(app);
     }
 
-    // 2. 메시지 전송  - 테스트용 id만
+    // 2. 테스트 - 비동기
     public void pushAlarmTest(Long familyId, String token) throws FirebaseMessagingException {
         log.info("알림 테스트 시작");
 
         MemoryRandomResponseDto memoryDto = memoryService.getRandomMemory(familyId);
 
-        FcmPushDto fcmPushDto = FcmPushDto.builder()
-                .memoryId(memoryDto.getMemoryId())
-                .memoryTitle(memoryDto.getTitle() + "추억이 기억나시나요?")
+        Notification notification = new Notification("Remember Me", memoryDto.getTitle() + PUSH_QUESTION);
+        Message message = Message.builder()
+                .setToken(token) // 사용자 디바이스 토큰
+                .setNotification(notification)
+                .putData("memoryId", memoryDto.getMemoryId().toString() )
                 .build();
 
-        Message message = createPushMessage(fcmPushDto, token);
-        this.firebaseMessaging.send(message);
+        this.firebaseMessaging.send(message); // 비동기
     }
 
+    // 2. 테스트 - 비동기
     public void pushAlarmTestAsync(Long familyId, String token) throws FirebaseMessagingException, ExecutionException, InterruptedException {
         log.info("알림 테스트 시작");
 
         MemoryRandomResponseDto memoryDto = memoryService.getRandomMemory(familyId);
 
-        FcmPushDto fcmPushDto = FcmPushDto.builder()
-                .memoryId(memoryDto.getMemoryId())
-                .memoryTitle(memoryDto.getTitle() + "추억이 기억나시나요?")
+        Notification notification = new Notification("Remember Me", memoryDto.getTitle() + PUSH_QUESTION);
+        Message message = Message.builder()
+                .setToken(token) // 사용자 디바이스 토큰
+                .setNotification(notification)
+                .putData("memoryId", memoryDto.getMemoryId().toString())
                 .build();
 
-        Message message = createPushMessage(fcmPushDto, token);
-        this.firebaseMessaging.sendAsync(message).get();
+        this.firebaseMessaging.sendAsync(message).get(); // 비동기
     }
 
     @Scheduled(cron = "0 0 09 * * ?")
-    public void pushMorningAlarm(Long familyId, String token) throws FirebaseMessagingException, ExecutionException, InterruptedException {
-        log.info("오전 알림");
+    public void pushMorningAlarm(Long familyId, String token) throws ExecutionException, InterruptedException {
         MemoryRandomResponseDto memoryDto = memoryService.getRandomMemory(familyId);
 
-        FcmPushDto fcmPushDto = FcmPushDto.builder()
-                .memoryId(memoryDto.getMemoryId())
-                .memoryTitle(memoryDto.getTitle() + "추억이 기억나시나요?")
+        Notification notification = new Notification("Remember Me", memoryDto.getTitle() + PUSH_QUESTION);
+        Message message = Message.builder()
+                .setToken(token) // 사용자 디바이스 토큰
+                .setNotification(notification)
+                .putData("memoryId", memoryDto.getMemoryId().toString())
                 .build();
 
-        Message message = createPushMessage(fcmPushDto, token);
         this.firebaseMessaging.sendAsync(message).get(); // 비동기
     }
 
@@ -102,24 +104,13 @@ public class FcmPushService {
         log.info("오후 알림");
         MemoryRandomResponseDto memoryDto = memoryService.getRandomMemory(familyId);
 
-        FcmPushDto fcmPushDto = FcmPushDto.builder()
-                .memoryId(memoryDto.getMemoryId())
-                .memoryTitle(memoryDto.getTitle() + "추억이 기억나시나요?")
-                .build();
-
-        Message message = createPushMessage(fcmPushDto, token);
-        this.firebaseMessaging.sendAsync(message).get(); // 비동기
-    }
-
-    // Message 형식으로 변경
-    private Message createPushMessage(FcmPushDto data, String token) {
-        Notification notification = new Notification("Remember Me", data.getMemoryTitle());
-
+        Notification notification = new Notification("Remember Me", memoryDto.getTitle() + PUSH_QUESTION);
         Message message = Message.builder()
-                .setToken(token) // 사용자 토큰
+                .setToken(token) // 사용자 디바이스 토큰
                 .setNotification(notification)
-                .putData("memoryId", data.getMemoryId().toString())
+                .putData("memoryId", memoryDto.getMemoryId().toString())
                 .build();
-        return message;
+
+        this.firebaseMessaging.sendAsync(message).get(); // 비동기
     }
 }
